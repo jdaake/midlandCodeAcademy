3cab0d18ff588b2b8db0dd215cf1eea3dcb5c27e
Mike <mike@miketruax.com>
2018-06-20 15:57:42 -0500

    Adds all Work for 6/20

---
+++ 06-20-18/Classwork/Soundboard/main.js
@@ -0,0 +1,7 @@
+// Bovine University - http://www.richmolnar.com/Sounds/Ralph%20-%20Bovine%20U.wav
+// Indeedily Doodily - http://www.richmolnar.com/Sounds/Ned%20-%20Indeedily%20doodily.wav
+// Okily Dokily - http://www.richmolnar.com/Sounds/Ned%20-%20Okily%20Dokily.wav
+// You're All Nuts - http://www.richmolnar.com/Sounds/Homer%20-%20You're%20all%20nuts.wav
+// Take This Job and Restaff It - http://www.richmolnar.com/Sounds/Homer%20-%20Take%20this%20job%20and.wav
+// Hi Everybody - http://www.richmolnar.com/Sounds/Dr.%20Nick%20-%20Hi%20everybody%20(1).wav
+// Cool I broke his brain - http://www.richmolnar.com/Sounds/Bart%20-%20Cool,%20I%20broke%20his%20brain.wav    
