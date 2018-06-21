fdd8df4e5f62500133b6eeecb3e356ce30e71e9c
miketruax <mike@miketruax.com>
2018-06-20 19:48:02 -0500

    Adds in class work

---
+++ 06-20-18/Classwork/MazeGame/main.js
@@ -0,0 +1,45 @@
+var size = {
+  height: window.innerHeight,
+  width: window.innerWidth
+}
+var gameStarted = false;
+for (var i = 0; i<75; i++){
+  var div = document.createElement("div");
+  div.style.position= "absolute";
+  div.style.top = Math.floor(Math.random() * (size.height- 30)) +"px";
+  div.style.left = Math.floor(Math.random() * (size.width- 30)) +"px";
+  div.addEventListener("mouseenter", function() {
+    if(gameStarted){
+      endGame("loser");
+    }
+  })
+  document.querySelector("body").appendChild(div);
+
+}
+
+var div = document.createElement("div");
+div.classList.add('start');
+div.style.position= "absolute";
+div.style.top = Math.floor(Math.random() * (size.height- 30)) +"px";
+div.style.left = Math.floor(Math.random() * (size.width- 30)) +"px";
+div.addEventListener("mouseenter", function() {
+  gameStarted = true;
+})
+document.querySelector("body").appendChild(div);
+
+var div = document.createElement("div");
+div.classList.add('end');
+div.style.position= "absolute";
+div.style.top = Math.floor(Math.random() * (size.height- 30)) +"px";
+div.style.left = Math.floor(Math.random() * (size.width- 30)) +"px";
+div.addEventListener("mouseenter", function() {
+  if(gameStarted){
+    endGame("winner");
+  }
+})
+document.querySelector("body").appendChild(div);
+
+function endGame(result){
+  gameStarted = false;
+  alert("You are a " + result );
+}
--- 06-20-18/Classwork/Soundboard/main.js
+++ 06-20-18/Classwork/Soundboard/main.js
@@ -1,7 +1,16 @@
-// Bovine University - http://www.richmolnar.com/Sounds/Ralph%20-%20Bovine%20U.wav
-// Indeedily Doodily - http://www.richmolnar.com/Sounds/Ned%20-%20Indeedily%20doodily.wav
-// Okily Dokily - http://www.richmolnar.com/Sounds/Ned%20-%20Okily%20Dokily.wav
-// You're All Nuts - http://www.richmolnar.com/Sounds/Homer%20-%20You're%20all%20nuts.wav
-// Take This Job and Restaff It - http://www.richmolnar.com/Sounds/Homer%20-%20Take%20this%20job%20and.wav
-// Hi Everybody - http://www.richmolnar.com/Sounds/Dr.%20Nick%20-%20Hi%20everybody%20(1).wav
-// Cool I broke his brain - http://www.richmolnar.com/Sounds/Bart%20-%20Cool,%20I%20broke%20his%20brain.wav    
+var sounds = {
+  "doodily": new Audio("http://www.richmolnar.com/Sounds/Ned%20-%20Indeedily%20doodily.wav"),
+  "bovine": new Audio("http://www.richmolnar.com/Sounds/Ralph%20-%20Bovine%20U.wav"),
+  "okily": new Audio("http://www.richmolnar.com/Sounds/Ned%20-%20Okily%20Dokily.wav"),
+  "nuts": new Audio("http://www.richmolnar.com/Sounds/Homer%20-%20You're%20all%20nuts.wav"),
+  "job": new Audio("http://www.richmolnar.com/Sounds/Homer%20-%20Take%20this%20job%20and.wav"),
+  "everybody": new Audio("http://www.richmolnar.com/Sounds/Dr.%20Nick%20-%20Hi%20everybody%20(1).wav"),
+  "broke": new Audio("http://www.richmolnar.com/Sounds/Bart%20-%20Cool,%20I%20broke%20his%20brain.wav")
+}
+var buttons = document.querySelectorAll(".button");
+
+for(var i = 0; i < buttons.length; i++){
+buttons[i].addEventListener('click', function(e){
+  sounds[e.target.id].play();
+})
+}
